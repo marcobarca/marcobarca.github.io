348d4c49b257a4f3688e3300e48e1bca32f01e3f
marco <marcobarca1995@gmail.com>
2023-09-15 13:17:53 +0200

    changing vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  base: '/marcobarca.github.io/', // Adjust this to match your GitHub Pages repository name
+};
