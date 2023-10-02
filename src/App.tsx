0a3e91ac9b1d3e2278507baa8875645fa255ed14
marco <marcobarca1995@gmail.com>
2023-10-02 22:03:05 +0200

    npm run build executed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import githubLogo from './assets/github.svg';
 import linkedinLogo from './assets/linkedin.svg';
 import alienImage from './assets/alien.png';
