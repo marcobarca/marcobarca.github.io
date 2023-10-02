3fdd05854076562f8fea147fe32e60eda7628295
marco <marcobarca1995@gmail.com>
2023-10-02 15:56:40 +0200

    update

--- vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default {
-  base: '/marcobarca.github.io/', // Adjust this to match your GitHub Pages repository name
-};
